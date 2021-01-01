bafabe3e974e1c654255a7c6b5211de8e10b425f
Hao Liu <harryliuhao@hotmail.com>
2020-12-31 22:00:20 -0500

    routing parameters

--- src/app/item-form/item-detail.component.ts
+++ src/app/item-form/item-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+
 import { Iitem } from './item';
 
 @Component({
@@ -10,9 +12,28 @@ export class ItemDetailComponent implements OnInit {
   pageTitle: string='Item Detail';
   item: Iitem|undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) { 
+                console.log(this.route.snapshot.paramMap.get('id'));
+              }
 
-  ngOnInit(): void {
+  ngOnInit(): void{
+    let id=this.route.snapshot.paramMap.get('id');
+    this.pageTitle+=':> '+id;
+    this.item={
+      "itemId": 3,
+      "itemName": "head lamp",
+      "room": "garage",
+      "place": "board",
+      "updateDate": "October 19, 2020",
+      "description": "new",
+      "quantity": 1,
+      "starRating": 5
+    }
+  }
+
+  onBack(): void{
+    this.router.navigate(['/items']);
   }
 
 }
--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -15,7 +15,6 @@ export class StarComponent implements OnChanges {
     
     ngOnChanges(): void {
         this.starWidth=this.rating*75/5;
-        console.log("why here works?", this.starWidth);
     }
 
     onClick(): void {
