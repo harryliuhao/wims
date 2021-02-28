10f8fa822556850894c145ac2314df04819b51df
Hao Liu <harryliuhao@hotmail.com>
2021-02-27 20:38:38 -0500

    observable

--- src/app/item-form/item-input.component.ts
+++ src/app/item-form/item-input.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ItemService} from './item.service';
 import {UserSettings} from '../data/user-settings';
-import { NgForm } from '@angular/forms';
+import { NgForm, NgModel } from '@angular/forms';
+import { DataService } from '../data/data.service';
 
 @Component({
   selector: 'app-item-input',
@@ -10,23 +11,45 @@ import { NgForm } from '@angular/forms';
 })
 export class ItemInputComponent implements OnInit {
 
-  userSettings: UserSettings={
+  originalUserSettings: UserSettings={
     itemName: '',
-    itemRoom: 'garage',
-    whereInRoom: 'rack',
+    itemRoom: 'basement',
+    whereInRoom: 'stair',
     itemUnit: 1
   };
-  
-  constructor(private itemService: ItemService) {
+
+  userSettings: UserSettings = {...this.originalUserSettings};
+  postError=false;
+  postErrorMessage='';
+
+  constructor(private dataService: DataService) {
     
   }
 
   ngOnInit(): void {
   }
 
-  onSubmit(form: NgForm) {
-    console.log('onSubmit status: ', form.valid);
+  onHttpError(errorResponse: any) {
+    console.log('error: ', errorResponse );
+    this.postError=true;
+    this.postErrorMessage=errorResponse.error.errorMessage;
   }
+  onSubmit(form: NgForm) {
+    console.log("in onSubmit: ", form.valid);
+    if(form.valid){
+      this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+        (      result: any) => console.log('success: ', result),
+        (      error: any) => this.onHttpError(error)
+      );
+    }
+    else {
+      this.postError=true;
+      this.postErrorMessage="Please fix the above errors";
+    }
+
+
+  }
+  
   onBlur(field: NgModel) {
     console.log('onBlur status: ', field.valid);
   }
